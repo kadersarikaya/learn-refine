e60d6a3b3253a5b48d9adb5fa66e31bdeeb796d6
unknown <kadersarikaya0123@gmail.com>
2022-08-19 06:35:43 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ import {
   ErrorComponent,
 } from "@pankod/refine-antd";
 import "@pankod/refine-antd/dist/styles.min.css";
+
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
+import { PostList  } from "./pages/posts/list";
+import { PostShow } from "./pages/posts/show";
+import { PostEdit } from "./pages/posts/edit";
+import { PostCreate } from "./pages/posts/create";
 
 function App() {
   return (
@@ -18,6 +23,13 @@ function App() {
       catchAll={<ErrorComponent />}
       routerProvider={routerProvider}
       dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[{ 
+        name: "posts", 
+        list: PostList,
+        show: PostShow,
+        edit: PostEdit, 
+        create: PostCreate,
+      canDelete: true,}]}
     />
   );
 }
